cc507cef5e1adeb143765996121990f122cfcd4d
Anene Praise <anenepraise1@gmail.com>
2025-12-07 19:20:45 +0100

    feat: begin implementation of card content section

--- src/components/Recipe/Card.tsx
+++ src/components/Recipe/Card.tsx
@@ -1,10 +1,12 @@
-import CardHeader from "./CardHeader"
-import classes from "./Card.module.css"
+import CardHeader from "./CardHeader";
+import CardContent from "./CardContent";
+import classes from "./Card.module.css";
 
 export default function Card() {
   return (
     <div className={classes.container}>
       <CardHeader />
+      <CardContent />
     </div>
-  )
+  );
 }
